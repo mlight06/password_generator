df3135c2de441dd50c826938fec8077fa9d82ad4
Michael Light <mklight06@gmail.com>
2023-10-24 13:39:51 -0600

    refactored to not use hard-coded list of alphabet

--- generator.py
+++ generator.py
@@ -1,10 +1,11 @@
 import random
 import re
+import string
 
-
-# want to comeback and refactor with regex
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
-# chars = re.compile('')
+characters = list(string.ascii_letters)
+numbers = list(range(0,9))
+stringNums = [str(num) for num in numbers]
+allchars = characters + stringNums
 
 numPasswords = int(input('How many passwords would you like to generate?'))
 passLength = int(input("How many characters do you need for your password?"))
@@ -14,7 +15,8 @@ print('\nHere are your passwords:')
 for password in range(numPasswords):
     passwords = ''
     for character in range(passLength):
-        passwords += random.choice(chars)
+        passwords += random.choice(allchars)
     print(passwords)
+
 
 # print('chars', chars)
