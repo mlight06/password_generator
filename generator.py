bb8d7f20039560a460700c693453a9a492736e6a
Michael Light <mklight06@gmail.com>
2023-10-24 13:14:53 -0600

    working as expected, will refactor

--- generator.py
+++ generator.py
@@ -1,3 +1,21 @@
 import random
+import re
 
-chars = ''
+
+# want to comeback and refactor with regex
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
+# chars = re.compile('')
+
+numPasswords = input('How many passwords would you like to generate?')
+numPasswords = int(numPasswords)
+passLength = int(input("How many characters do you need for your password?"))
+
+print('\nHere are your passwords:')
+
+for password in range(numPasswords):
+    passwords = ''
+    for character in range(passLength):
+        passwords += random.choice(chars)
+    print(passwords)
+
+# print('chars', chars)
